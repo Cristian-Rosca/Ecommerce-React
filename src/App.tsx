89ad0e014d74c44465b0f6d2111ae867b83a6e26
Cristian Rosca <cristianrrosca@gmail.com>
2022-12-17 21:05:55 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Beginner Programs</h2>
+          <p>View Programs</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Intermediate Programs</h2>
+          <p>View Programs</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Advanced Programs</h2>
+          <p>View Programs</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Hypertrophy</h2>
+          <p>View Programs</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Strength</h2>
+          <p>View Programs</p>
+        </div>
+      </div>
+     
     </div>
   );
 }
