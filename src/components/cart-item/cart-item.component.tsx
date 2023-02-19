e0e1d146df81e1837a0f01d2a9e78f8edfcd9c8c
Cristian Rosca <cristianrrosca@gmail.com>
2023-02-19 11:55:00 +0000

    change usd to eur

--- src/components/cart-item/cart-item.component.tsx
+++ src/components/cart-item/cart-item.component.tsx
@@ -9,7 +9,7 @@ const CartItem = ({ cartItem }: any) => {
             <img src={imageUrl} alt={name} />
             <div className='item-details'>
                 <span className='name'>{name}</span>
-                <span className='price'>{quantity} x ${price}</span>
+                <span className='price'>{quantity} x €{price}</span>
             </div>
         </div>
     );
